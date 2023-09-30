7df99b8761d461fad537d8bce9a5ca1ce8c8df87
codenamenam <bluetire27@gmail.com>
2023-09-30 18:45:26 +0900

    fix: Html size

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" style={{ height: "700px" }}>
       <Head />
-      <body style={{ height: "700px" }}>
+      <body>
         <Main />
         <NextScript />
       </body>
